978be82ed1c4bd45a1689d53b09f82739f6cd1bb
aismaniotto <aismaniotto@gmail.com>
2020-10-12 00:05:51 -0300

    Create mobile project and basic setup.

---
+++ mobile/mobile/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+using mobile.Presentation.Pages;
+
+namespace mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
